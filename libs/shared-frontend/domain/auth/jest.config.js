e4b3ed5a44d2a7467ee54f426dc5f66d96f51e4e
Przemysław Żydek <przemyslawzydek@gmail.com>
2021-04-07 09:26:06 +0200

    test: Fix tests

--- libs/shared-frontend/domain/auth/jest.config.js
+++ libs/shared-frontend/domain/auth/jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   displayName: 'shared-frontend-domain-auth',
   preset: '../../../../jest.preset.js',
   transform: {
-    '^.+\\.[tj]sx?$': 'babel-jest',
+    '^.+\\.[tj]sx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../../../coverage/libs/shared-frontend/domain/auth',
