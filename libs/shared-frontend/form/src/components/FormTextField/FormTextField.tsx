5d27f3fcb8114dab4f704c77e7f7a53d31c39a7f
Przemysław Żydek <przemyslawzydek@gmail.com>
2021-04-08 12:47:37 +0200

    refactor: Use "useController"

--- libs/shared-frontend/form/src/components/FormTextField/FormTextField.tsx
+++ libs/shared-frontend/form/src/components/FormTextField/FormTextField.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { TextField, TextFieldProps } from '@material-ui/core';
-import { ControllerProps, Controller } from 'react-hook-form';
+import { ControllerProps, useController } from 'react-hook-form';
 
 export interface FormTextFieldProps
   extends Omit<TextFieldProps, 'name'>,
@@ -13,25 +13,23 @@ export const FormTextField = ({
   name,
   ...rest
 }: FormTextFieldProps) => {
+  const {
+    field,
+    fieldState: { error },
+  } = useController({
+    name,
+    rules,
+    control,
+    defaultValue,
+  });
+
   return (
-    <Controller
-      defaultValue={defaultValue}
-      rules={rules}
-      control={control}
-      name={name}
-      render={(props) => (
-        <TextField
-          {...rest}
-          {...props.field}
-          variant={rest.variant}
-          error={Boolean(props.fieldState.error)}
-          helperText={
-            props.fieldState.error
-              ? props.fieldState.error.message
-              : rest.helperText
-          }
-        />
-      )}
+    <TextField
+      {...rest}
+      {...field}
+      variant={rest.variant}
+      error={Boolean(error)}
+      helperText={error ? error.message : rest.helperText}
     />
   );
 };
