4d443599965aed30a56a03568a9fbd8cd79724ff
Przemysław Żydek <przemyslawzydek@gmail.com>
2021-04-09 10:39:46 +0200

    chore: Add more components for old app

---
+++ libs/shared-frontend/ui/src/molecules/SelectablePaper/SelectablePaper.tsx
@@ -0,0 +1,52 @@
+import React, { MouseEventHandler, ReactNode } from 'react';
+import { Box, BoxProps, Paper, PaperProps } from '@material-ui/core';
+import classNames from 'classnames';
+import { useStyles } from './SelectablePaper.styles';
+
+export interface SelectablePaperProps {
+  checked?: boolean;
+  disabled?: boolean;
+  onClick?: MouseEventHandler;
+  width?: number | string;
+  height?: number | string;
+  className?: string;
+  children?: ReactNode;
+  boxProps?: BoxProps;
+  paperProps?: PaperProps;
+}
+
+export const SelectablePaper = ({
+  children,
+  disabled,
+  checked,
+  onClick,
+  height = '100px',
+  width = '100px',
+  className,
+  boxProps,
+  paperProps,
+}: SelectablePaperProps) => {
+  const classes = useStyles();
+
+  return (
+    <Box width={width} height={height} className={className} {...boxProps}>
+      <Paper
+        {...paperProps}
+        role="radio"
+        onClick={disabled ? undefined : onClick}
+        className={classNames(
+          {
+            checked,
+            disabled,
+          },
+          'selectable-paper',
+          classes.paper,
+          paperProps?.className
+        )}
+        variant="outlined"
+      >
+        {children}
+      </Paper>
+    </Box>
+  );
+};
---
+++ libs/shared-frontend/ui/src/molecules/TileRadio/TileRadio.types.ts
@@ -0,0 +1,7 @@
+import { ReactNode } from 'react';
+import { SelectablePaperProps } from '../SelectablePaper/SelectablePaper';
+
+export interface TileRadioProps extends SelectablePaperProps {
+  title: string;
+  icon?: ReactNode;
+}
