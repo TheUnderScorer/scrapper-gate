cba8657f12e9cbcbb00e1e9175fcfce0c083d2a6
Przemysław Żydek <przemyslawzydek@gmail.com>
2021-06-23 07:54:34 +0200

    test: Fix tests

--- libs/backend/domain/scrapper/jest.config.js
+++ libs/backend/domain/scrapper/jest.config.js
@@ -9,6 +9,7 @@ module.exports = {
   transform: {
     '^.+\\.[tj]sx?$': 'ts-jest',
   },
+  testPathIgnorePatterns: ['playwright-runner'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../../../coverage/libs/backend/domain/scrapper',
   setupFilesAfterEnv: [`${process.cwd()}/tests/setupBackend.ts`],
