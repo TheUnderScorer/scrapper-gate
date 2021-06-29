51123f8e07838f16da43581f98c9a48bda632958
Przemysław Żydek <przemyslawzydek@gmail.com>
2021-06-29 08:09:14 +0200

    ci: Wait for localstack in docker step

--- deploy/infra/variables.tf
+++ deploy/infra/variables.tf
@@ -2,6 +2,14 @@ variable "env_name" {
   default = "staging"
 }
 
+variable "app_name" {
+  default = "Scrapper Gate"
+}
+
 variable "dev_services_count" {
+  default = 0
+}
+
+variable "premium_localstack_services_count" {
   default = 0
 }
