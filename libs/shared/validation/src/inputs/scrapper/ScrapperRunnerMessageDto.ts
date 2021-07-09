7dec218787883cf7cd27391d241d2273c68839a0
Przemysław Żydek <przemyslawzydek@gmail.com>
2021-07-09 08:04:46 +0200

    chore: Fix invalid import

--- libs/shared/validation/src/inputs/scrapper/ScrapperRunnerMessageDto.ts
+++ libs/shared/validation/src/inputs/scrapper/ScrapperRunnerMessageDto.ts
@@ -1,5 +1,5 @@
 import { ScrapperRunnerMessagePayload } from '@scrapper-gate/shared/domain/scrapper';
-import { RunnerTrigger } from '../../BaseSchema';
+import { RunnerTrigger } from '@scrapper-gate/shared/schema';
 import { BaseSchema } from '../../BaseSchema';
 import { requiredEnum } from '../../decorators/enum';
 import { uuid } from '../../decorators/uuid';
