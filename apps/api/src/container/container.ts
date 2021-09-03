36df8bf235f1b502c2e963204daf5ceee4d50c85
Przemysław Żydek <przemyslawzydek@gmail.com>
2021-09-03 11:45:24 +0200

    chore: Update apollo server to 3

--- apps/api/src/container/container.ts
+++ apps/api/src/container/container.ts
@@ -123,6 +123,8 @@ export const createContainer = async ({
 
   const apolloServer = container.resolve<ApolloServer>('apolloServer');
 
+  await apolloServer.start();
+
   server.addHook('preHandler', async (req) => {
     req.container = container;
   });
