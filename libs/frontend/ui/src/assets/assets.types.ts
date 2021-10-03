0f41eeebe0b7719346f810da384ca47628f1196b
Przemysław Żydek <przemyslawzydek@gmail.com>
2021-10-03 13:18:55 +0200

    chore: Temporal fix for type issue

--- libs/frontend/ui/src/assets/assets.types.ts
+++ libs/frontend/ui/src/assets/assets.types.ts
@@ -1,4 +1,8 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+
+// TODO Figure out the types issue in other libs
 export const getAssetsMap = {
+  // @ts-ignore
   notFoundSolid: () => import('./not found_solid III.svg'),
 };
 
