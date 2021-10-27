b902d2add9d21ed5854125ba7fca009211dc593d
Przemysław Żydek <przemyslawzydek@gmail.com>
2021-10-27 14:00:34 +0200

    chore: Update nx to 13

--- libs/frontend/ui/.storybook/main.js
+++ libs/frontend/ui/.storybook/main.js
@@ -5,5 +5,6 @@ const rootMain = require('../../../../.storybook/main');
 rootMain.stories.push(
   ...['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)']
 );
+rootMain.core = { ...rootMain.core, builder: 'webpack5' };
 
 module.exports = rootMain;
